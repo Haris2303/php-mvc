b8ad5326350f93efc4f268c2c69826a9e5465a48
haris2303 <hrsccf102@gmail.com>
2023-09-22 11:18:58 +0900

    Controller

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Belajar\PHP\MVC\App\Router;
+use Belajar\PHP\MVC\Controller\HomeController;
 
-Router::add('GET', '/', 'HomeController', 'index');
-Router::add('GET', '/login', 'LoginController', 'login');
-Router::add('GET', '/register', 'RegisterController', 'register');
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+Router::add('GET', '/world', HomeController::class, 'world');
 
 Router::run();
